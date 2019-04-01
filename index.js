397c0119c1644c969af78b5b2184c742c57c27fc
mhmmd-ysf <muhammad_yusuf22@outlook.co.id>
2019-04-01 19:06:14 +0700

    Update config

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const route = require('./routes')
+const fs = require('fs')
+
+const config = {
+  "production": {
+    "username": "postgres",
+    "password": process.env.PASSWORD,
+    "database": "phase2Student",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  }
+}
+fs.writeFileSync( './config/config.json',JSON.stringify(config, null, 2), 'utf8')
 
 app.set('view engine', 'ejs')
 app.use(express.json())
